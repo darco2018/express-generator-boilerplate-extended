a8610f0c7224a647df9c810697236294feba42fe
darco2018 <newsecond@outlook.com>
2019-09-23 16:06:00 +0200

    add body-parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,9 +16,13 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); //vs urlencoded for body-parser?!
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// move to routers?
+app.use(bodyParser.urlencoded({ extended: 'true' })); // const requestBodyStr = JSON.stringify(req.body);
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
