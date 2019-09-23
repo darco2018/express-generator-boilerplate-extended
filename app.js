e8a4488439cede27243c86c854c38f08e4142eab
darco2018 <newsecond@outlook.com>
2019-09-23 18:23:26 +0200

    Add movies.js/ejs -request package for API

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: 'true' })); // const requestBodyStr = 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/movies', moviesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/movies.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+const express = require('express');
+const request = require('request');
+
+const router = express.Router();
+const OMDbAPIkey = '111ffc84';
+const urlBase = `http://www.omdbapi.com/?apikey=${OMDbAPIkey}&`; // + 's=polish';
+
+function Movie(title, year) {
+  this.title = title;
+  this.year = year;
+}
+// const inMemoryMovieDb = [new Movie('Film 1', '1923'), new Movie('Film 2', '1923'), new Movie('Film 3', '1923')];
+/* GET /movies page. */
+router.get('/', (req, res) => {
+  const queryParams = 's=polish'; // !!!
+  const url = urlBase + queryParams;
+
+  request(url, (error, response, body) => {
+    console.log(`error: ${error}`);
+    console.log(`status code: ${response}` && response.statusCode);
+    console.log(`body: ${body.substring(0, 60)}`);
+    console.log(typeof body); // string
+    const moviesObj = JSON.parse(body); // now: object
+
+    res.render('movies', { movies: moviesObj });
+  });
+  // res.render('movies', { movies: inMemoryMovieDb });
+});
+
+router.post('/', (req, res) => {
+  /* console.log(req.body);
+  const search = req.body.searchword;
+  const queryParams = !search ? 's=polish' : `s=${search}`; // !!!
+  const url = urlBase + queryParams;
+
+  request(url, (error, response, body) => {
+    console.log(`error: ${error}`);
+    console.log(`status code: ${response}` && response.statusCode);
+    console.log(`body: ${body.substring(0, 60)}`);
+    console.log(typeof body); // string
+    const moviesObj = JSON.parse(body); // now: object
+
+    res.render('movies', { movies: moviesObj }); 
+  });*/
+});
+
+router.get('/new', (req, res) => {
+  /* res.render('new'); */
+});
+
+
+module.exports = router;
